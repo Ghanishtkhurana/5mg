8336f019542710c9584f92643aff1a3b8bb48387
Ghanishtkhurana <101393463+Ghanishtkhurana@users.noreply.github.com>
2023-01-17 21:03:08 +0530

    lab

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import AllRoutes from './pages/AllRoutes';
 
 function App() {
   return (
     <div className="App">
-      Homepage
+      <AllRoutes />
     </div>
   );
 }
