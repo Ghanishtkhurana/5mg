655a0a2bc8eb0b897b1388f99c5601471bb33579
sumansauravmay <sumansauravmay12345@gmail.com>
2023-01-19 00:05:18 +0530

    search part

--- frontend/src/components/HomeComponents/Search.jsx
+++ frontend/src/components/HomeComponents/Search.jsx
@@ -1,12 +1,25 @@
 import React from 'react';
-
+import { Input,Flex,Box, Button } from '@chakra-ui/react'
 
 const Search = () => {
-  return (
-    <div>
+   
+  
+    return (
+        <Box border="1px solid" p="20px">
+        <Flex color='white' alignItems="center" justifyContent="space-between">
+
+        <Input p="20px"
+          pr='4.5rem'
+          type="text"
+          placeholder='Search Products'
+        />
+  <Box>
+    <Button bg='tomato'>Quick order</Button>
+  </Box>
+</Flex>
+        </Box>
       
-    </div>
-  )
+    )
 }
 
 export default Search
