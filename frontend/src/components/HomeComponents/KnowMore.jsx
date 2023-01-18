de04eedf264ce9c00d5c413c7e0cbb586b2e61a4
Vivek-bhatt05 <vivekbhatt529@gmail.com>
2023-01-19 00:04:31 +0530

    added Know more

---
+++ frontend/src/components/HomeComponents/KnowMore.jsx
@@ -0,0 +1,16 @@
+import { Box ,Container,Image} from '@chakra-ui/react'
+import React from 'react'
+
+const KnowMore = () => {
+  return (
+    <div>
+     <Box backgroundColor='#f6f6f6' paddingY='10px'>
+        <Container maxW="1300px">
+       <Image src='https://onemg.gumlet.io/marketing/29087450-0f25-4856-96fc-62f4ea81bbd3.png' alt='Dan Abramov' />
+        </Container>
+     </Box>
+    </div>
+  )
+}
+
+export default KnowMore
