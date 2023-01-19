1db36aeb9e8fb3edd4a48f872699dd0db4eb1679
meenakshi singh <meenakshisinghthakur4668@gmail.com>
2023-01-19 20:57:12 +0530

    authantication_Day_3

--- backend/index.js
+++ backend/index.js
@@ -1,52 +1,55 @@
-const express=require("express")
-const cors = require("cors")
-const { connection } = require("./config/db")
-const { fullBodyCheckRouter } = require("./routes/FullBodyCheck.route")
-const { comboDealsRouter } = require("./routes/ComboDeals.route")
-const { trendingRouter } = require("./routes/Trending.route")
-const { cerealRouter } = require("./routes/Cereal.route")
-const { plantRouter } = require("./routes/PlantBases.route")
+const express = require("express");
+const cors = require("cors");
+const { connection } = require("./config/db");
+const { fullBodyCheckRouter } = require("./routes/FullBodyCheck.route");
+const { comboDealsRouter } = require("./routes/ComboDeals.route");
+const { trendingRouter } = require("./routes/Trending.route");
+const { cerealRouter } = require("./routes/Cereal.route");
+const { plantRouter } = require("./routes/PlantBases.route");
 
-const {AyurvedaRoute} = require("./routes/Ayurved.routes")
-const {SkinRoute} =require("./routes/Skin.routes")
-const {DigestRoute} =require("./routes/Digestivecare.routes")
-const {HairRoute} =require("./routes/Hair.routes")
-const {HerbsRoute} =require("./routes/Herbs.routes")
+const { AyurvedaRoute } = require("./routes/Ayurved.routes");
+const { SkinRoute } = require("./routes/Skin.routes");
+const { DigestRoute } = require("./routes/Digestivecare.routes");
+const { HairRoute } = require("./routes/Hair.routes");
+const { HerbsRoute } = require("./routes/Herbs.routes");
+const { authantication } = require("./middlewares/auth.middleware");
 
-require('dotenv').config()
+require("dotenv").config();
 
+const app = express();
+app.use(
+  cors({
+    origin: "*",
+  })
+);
+app.use(express.json());
 
-const app=express()
-app.use(cors({
-    origin:"*"
-}))
-app.use(express.json())
+app.get("/", (req, res) => {
+  res.send("Home Page");
+});
+app.use(authantication);
 
-app.get("/",(req,res)=>{
-    res.send("Home Page")
-}) 
-
-app.use("/fullBodyCheck",fullBodyCheckRouter)
-app.use("/comboDeals",comboDealsRouter)
-app.use("/trending",trendingRouter)
-app.use("/cereal",cerealRouter)
-app.use("/plant",plantRouter)
+app.use("/fullBodyCheck", fullBodyCheckRouter);
+app.use("/comboDeals", comboDealsRouter);
+app.use("/trending", trendingRouter);
+app.use("/cereal", cerealRouter);
+app.use("/plant", plantRouter);
 
 // ayurveda page-----
-app.use("/herbs",HerbsRoute)
-app.use("/skin",SkinRoute)
-app.use("/digest",DigestRoute)
-app.use("/hairs",HairRoute)
-app.use("/ayurveda",AyurvedaRoute)
+app.use("/herbs", HerbsRoute);
+app.use("/skin", SkinRoute);
+app.use("/digest", DigestRoute);
+app.use("/hairs", HairRoute);
+app.use("/ayurveda", AyurvedaRoute);
 // ayurveda page-----
 
-app.listen(4001,async()=>{
-    try{
-        await connection
-        console.log("Connected to database")
-    }catch(err){
-        console.log("Problem connecting")
-        console.log(err)
-    }
-    console.log("Port running")
-})
+app.listen(4001, async () => {
+  try {
+    await connection;
+    console.log("Connected to database");
+  } catch (err) {
+    console.log("Problem connecting");
+    console.log(err);
+  }
+  console.log("Port running");
+});
