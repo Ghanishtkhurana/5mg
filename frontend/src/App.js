2485303a4457b44dc22be0b0c7499bf1aa8b26a3
PRATEEK SONI <nmprateek9425@gmail.com>
2023-01-20 13:02:56 +0530

    carausel

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3,17 +3,24 @@ import AllRoutes from "./pages/AllRoutes";
 import Navbar from "./components/Navbar";
 import Footer from "./components/Footer";
 import { ChakraProvider } from "@chakra-ui/react";
+import { Carausel } from "./components/Carausel";
+import { Ayurved } from "./pages/Ayurved_0381";
+
 
 function App() {
   return (
     <div className="App">
-      <ChakraProvider>
+      {/* <ChakraProvider>
       <Navbar />
       </ChakraProvider>
+
       <AllRoutes />
+
       <ChakraProvider>
       <Footer />
-      </ChakraProvider>
+      </ChakraProvider> */}
+      <Ayurved/>
+    
     </div>
   );
 }
